1c68bbb527088179b5c7f4a9da17688caef35d71
meocakir <cakiroglu.mert@gmail.com>
2023-09-11 18:12:30 -0400

    Added multithreading and other columns

--- ParseDataReplaceBg.py
+++ ParseDataReplaceBg.py
@@ -1,7 +1,8 @@
 from utils.PropertyNames import ColumnNames as Cols
+from multiprocessing import Pool, cpu_count
 
 import pandas as pd
-
+import pickle as pkl
 
 def add_date_to_time_column(patient_mask, time_column_name, starting_date):
     # Convert the starting date to a datetime object
@@ -11,7 +12,7 @@ def add_date_to_time_column(patient_mask, time_column_name, starting_date):
     prev_hour = 0
 
     # Iterate through rows and update the time column
-    for index, row in df[patient_mask].iterrows():
+    for index, row in replaceBgDf[patient_mask].iterrows():
         # Convert the time to a Timedelta for arithmetic
         current_hour = row[time_column_name].hour
 
@@ -19,7 +20,7 @@ def add_date_to_time_column(patient_mask, time_column_name, starting_date):
         if current_hour < prev_hour:
             current_date += pd.Timedelta(days=1)
 
-        df.at[index, time_column_name] = pd.Timestamp(current_date.date()).replace(
+        replaceBgDf.at[index, time_column_name] = pd.Timestamp(current_date.date()).replace(
             hour=row[time_column_name].hour,
             minute=row[time_column_name].minute,
             second=row[time_column_name].second)
@@ -27,40 +28,64 @@ def add_date_to_time_column(patient_mask, time_column_name, starting_date):
         # Update the DataFrame
         prev_hour = current_hour
 
+
+def process_patient(patient_tuple):
+    i, patient_param = patient_tuple
+    print(f'Starting {patient_param} ({i + 1}/{len(patients)})...')
+    add_date_to_time_column(replaceBgDf[Cols.patient] == patient_param, Cols.date, start_date)
+    print(f'{patient_param} ({i + 1}/{len(patients)}) Just Ended!')
+
 file_path = 'C:/Users/cakir/OneDrive/Belgeler/Datasets/REPLACE-BG/Data Tables/HDeviceCGM.txt'
 
 delimiter = '|'
 
 print('Starting...')
 # Read the data into a DataFrame
-df = pd.read_csv(file_path, sep=delimiter)
+replaceBgDf = pd.read_csv(file_path, sep=delimiter)
 columns_to_drop = ['RecID', 'ParentHDeviceUploadsID', 'SiteID', 'DeviceDtTmDaysFromEnroll',
                    'DexInternalDtTmDaysFromEnroll', 'DexInternalTm']
-df.drop(columns=columns_to_drop, inplace=True)
-df.rename(columns={'PtID': Cols.patient, 'DeviceTm': Cols.date, 'GlucoseValue': Cols.value}, inplace=True)
-df[Cols.date] = pd.to_datetime(df[Cols.date], format='%H:%M:%S').dt.time
-patients = df[Cols.patient].unique()
+replaceBgDf.drop(columns=columns_to_drop, inplace=True)
+replaceBgDf.rename(columns={'PtID': Cols.patient, 'DeviceTm': Cols.date, 'GlucoseValue': Cols.value}, inplace=True)
+replaceBgDf[Cols.date] = pd.to_datetime(replaceBgDf[Cols.date], format='%H:%M:%S').dt.time
+patients = replaceBgDf[Cols.patient].unique()
 
-print(len(patients), len(df))
+print(len(patients), len(replaceBgDf))
 
 """
 Delete me for final use
 """
-patients = patients[:3]
-df = df[df[Cols.patient].isin(patients)]
-df = df.iloc[::-1].reset_index(drop=True)
+#patients = patients[:1]
+#replaceBgDf = replaceBgDf[replaceBgDf[Cols.patient].isin(patients)]
 
+replaceBgDf = replaceBgDf.iloc[::-1].reset_index(drop=True)
 
 print(len(patients), patients)
 
 start_date = '2023-01-01'  # Dummy date
-for i, patient in enumerate(patients):
-    print(f'Starting {patient} ({i + 1}/{len(patients)})...')
-    add_date_to_time_column(df[Cols.patient] == patient, Cols.date, start_date)
+num_cpus = cpu_count()
+print('Thread count:', num_cpus)
+with Pool(num_cpus) as p:
+    p.map(process_patient, enumerate(patients))
 
-for patient in patients:
-    print(patient)
-    print(df[df[Cols.patient] == patient].head())
-
-print(df.columns)
+print(replaceBgDf.columns)
 print('Done!')
+
+# TODO: Convert to timedelta
+# Define `date` gap
+replaceBgDf[Cols.date_gap] = replaceBgDf[Cols.date].diff()
+
+# Define `diff` column
+replaceBgDf[Cols.diff] = replaceBgDf[Cols.value].diff()
+
+custom_bins = [0, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 500]
+replaceBgDf[Cols.disc] = pd.cut(replaceBgDf[Cols.value], bins=custom_bins)
+replaceBgDf[Cols.isDangerous] = replaceBgDf[Cols.value] < 70
+
+# Before defining the char `char` column we sort the intervals, so we can define `char` based on the `value`
+intervals = sorted(replaceBgDf[Cols.disc].unique().tolist(), key=lambda x: x.left)
+interval_dict = {interval: index for index, interval in enumerate(intervals)}
+replaceBgDf[Cols.char] = replaceBgDf[Cols.disc].apply(lambda disc_row: interval_dict[disc_row])
+
+with open('/Data/REPLACE_BG.dat', 'wb') as file:
+    pkl.dump(replaceBgDf, file)
+
